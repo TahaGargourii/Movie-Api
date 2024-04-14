05c2cfb30d9ba56c5c9a8a447a96977b318eddca
TahaGargourii <Gargouriii.taha@gmail.com>
2024-04-15 00:09:00 +0200

    review services

--- src/main/java/dev/taha/movies/ReviewController.java
+++ src/main/java/dev/taha/movies/ReviewController.java
@@ -1,12 +1,25 @@
 package dev.taha.movies;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
-@RequestMapping("api/v1/movies")
+@RequestMapping("api/v1/reviews")
 public class ReviewController {
+
+
+    @Autowired
+    private ReviewService reviewService;
+
+    @PostMapping
+    public ResponseEntity<Review> createReview(@RequestBody Map<String ,String> payload) {
+        return new ResponseEntity<Review>(reviewService.createReview(payload.get("reviewBody" ) ,payload.get("imdbId")), HttpStatus.CREATED) ;
+
+    }
 
 
 }
